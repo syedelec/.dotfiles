361b535d37768354f19427d52dffdfbb2942c840
Sid-Ali Teir <teir.sidali@gmail.com>
2023-03-02 04:48:04 +0100

    bash: ps1: use grey for git branch

--- bash/.bashrc.d/10_ps1.bashrc.sh
+++ bash/.bashrc.d/10_ps1.bashrc.sh
@@ -7,10 +7,12 @@
 # trim dir in PS1 if exceeds number of folder
 export PROMPT_DIRTRIM=5
 
+# colorterm 256
 RST="\[\033[0m\]"
 RED="\[\033[31m\]"
 YEL_1="\[\033[38;5;179m\]"
 PRL="\[\033[35m\]"
+GRY="\[\033[38;5;247m\]"
 
 _git_ps1() {
     git rev-parse HEAD &> /dev/null || return
@@ -38,8 +40,8 @@ _git_ps1() {
 }
 
 if [ $EUID -eq 0 ]; then
-    PS1="> ${RED}\u ${YEL_1}\w${PRL} \`_git_ps1\`${RED}» ${RST}"
+    PS1="> ${RED}\u ${YEL_1}\w${GRY} \`_git_ps1\`${RED}» ${RST}"
 else
-    PS1="> ${YEL_1}\w${PRL} \`_git_ps1\`${RED}» ${RST}"
+    PS1="> ${YEL_1}\w${GRY} \`_git_ps1\`${RED}» ${RST}"
 fi
 
