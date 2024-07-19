1401e8e6db012a3f62cec21d4da4263b96e73a9b
Sid-Ali Teir <teir.sidali@gmail.com>
2024-07-19 12:45:17 +0200

    bash: add 'dds' completion

--- bash/.bashrc.d/20_completions.bashrc.sh
+++ bash/.bashrc.d/20_completions.bashrc.sh
@@ -5,11 +5,11 @@
 #######################################################################
 
 # make sure to source bash_completion
-# source /usr/share/bash-completion/bash_completion
+source /usr/share/bash-completion/completions/dd
 
 # _completion_loader dd
 if command -v dds > /dev/null 2>&1; then
     _completion_loader dd
-    complete -F _dd -o nospace dds
+    complete -F _comp_cmd_dd -o nospace dds
 fi
 
